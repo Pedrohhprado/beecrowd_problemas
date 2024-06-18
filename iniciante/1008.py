f9a9aec60f64878ecb5b0b8ff83ee119ddd78329
Pedrohhprado <pedrohhprado@gmail.com>
2024-06-18 09:14:04 -0300

    segundo commit

---
+++ iniciante/1008.py
@@ -0,0 +1,8 @@
+num_funcionario = int(input())
+num_hr_funcionario = int(input())
+hr_salario_funcionario = float(input())
+
+salario = num_hr_funcionario*hr_salario_funcionario
+
+print('NUMBER = {}'.format(num_funcionario))
+print('SALARY = U$ {:.2f}'.format(salario))
